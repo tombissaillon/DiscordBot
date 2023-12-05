e7c81476672e2fa7027b6538401840674b68cca8
mmangano23 <144923884+mmangano23@users.noreply.github.com>
2023-12-04 20:52:15 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-#Invite link: https://discord.com/api/oauth2/authorize?client_id=1181265740167401583&permissions=8&scope=bot
-#Token: MTE4MTI2NTc0MDE2NzQwMTU4Mw.GHsZaZ.qMRHobE4CFisQpsgklBue5tjdiYFHvZmFkqK0o
-
-
-#https://stackoverflow.com/questions/64261546/how-to-solve-error-microsoft-visual-c-14-0-or-greater-is-required-when-inst
-#https://stackoverflow.com/questions/44951456/pip-error-microsoft-visual-c-14-0-is-required
-#cmd command: pip install discord.py
+#https://xlsxwriter.readthedocs.io/getting_started.html#getting-started
 
 import discord
+
+import xlsxwriter
+
 from discord.ext import commands
 
 intents = discord.Intents.all()
@@ -15,14 +12,69 @@ intents.presences = False
 intents.messages = True
 intents.guilds = True
 
-bot = commands.Bot(command_prefix = "!", intents = intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
+#Excel File Setup
+workbook = xlsxwriter.Workbook('RegUsers.xlsx')
+
+worksheet = workbook.add_worksheet()
+
+worksheet.write('A1', 'Registered Users')
+worksheet.write('C1', 'Points')
+worksheet.write('E1', 'Tasks Completed')
+
+workbook.close()
+
+#print online message when bot is running
 @bot.event
 async def on_ready():
     print("BOT ONLINE")
 
+
+
+
+
+#Bot commands
+
+#TEST
 @bot.command()
 async def hello(ctx):
     await ctx.send("Hi!")
 
-bot.run("MTE4MTI2NTc0MDE2NzQwMTU4Mw.GHsZaZ.qMRHobE4CFisQpsgklBue5tjdiYFHvZmFkqK0o")
+#Register user class
+def registration(name):
+    
+    workbook = xlsxwriter.Workbook('RegUsers.xlsx')
+
+    #converts to char for excel writer
+    user = ('' + name)
+    nameCol = 'A2'
+    worksheet.write(nameCol, user)
+
+    workbook.close()
+
+#register user
+@bot.command()
+async def register(ctx, name):
+    users = registration(name)
+    await ctx.send("registered " + name)
+
+
+
+
+
+
+
+
+#print excel file
+@bot.command()
+async def print(ctx):
+    await ctx.send(file = discord.File('RegUsers.xlsx'))
+
+
+
+
+
+
+#bot token
+bot.run("MTE4MTI2NTc0MDE2NzQwMTU4Mw.GY2-9k.cb4yL9-qtTLj9SHdhtkcT1JVQhSdQoKZAJpGqg")
