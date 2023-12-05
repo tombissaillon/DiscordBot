3bf8653a295cfe2ef5a7a3bad0943fb5c2c1fdd2
mmangano23 <144923884+mmangano23@users.noreply.github.com>
2023-12-04 21:29:38 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-#https://xlsxwriter.readthedocs.io/getting_started.html#getting-started
+#TO DO
+    #Current Errors: why file will not update when information is added.
+    #Need to work on command to add points to users when task completed.
+    #Need to keep stamps and logs of what task and when completed.
+    #Greeting Message?
+
+#DONE
+    #list of commands
+    #Change bot commands to also accept / when inputted into chat, and to be seen when entering commands
+    #Rename bot biography
+
+
+#LIBRARY IMPORTS **IMPORTANT INFORMATION TO COMMITING TO CODE**
+    #https://stackoverflow.com/questions/64261546/how-to-solve-error-microsoft-visual-c-14-0-or-greater-is-required-when-inst
+    #https://stackoverflow.com/questions/44951456/pip-error-microsoft-visual-c-14-0-is-required
+    #cmd command: pip install discord.py
+
+    #https://xlsxwriter.readthedocs.io/getting_started.html#getting-started
 
 import discord
 
@@ -12,34 +29,44 @@ intents.presences = False
 intents.messages = True
 intents.guilds = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix= ["!","/"], intents=intents)
 
-#Excel File Setup
-workbook = xlsxwriter.Workbook('RegUsers.xlsx')
-
-worksheet = workbook.add_worksheet()
-
-worksheet.write('A1', 'Registered Users')
-worksheet.write('C1', 'Points')
-worksheet.write('E1', 'Tasks Completed')
-
-workbook.close()
-
-#print online message when bot is running
+#On bot start
 @bot.event
 async def on_ready():
+    #print online message to terminal when bot is running
     print("BOT ONLINE")
 
+    #Displays bot status
+    await bot.change_presence(activity=discord.Game(name="Type /commands_help for help"))
+
+    global workbook
+    workbook = xlsxwriter.Workbook('RegUsers.xlsx')
+    global worksheet 
+    worksheet = workbook.add_worksheet()
+    
+    #Setting up workbook
+    worksheet.write('A1', 'Registered Users')
+    worksheet.write('C1', 'Points')
+    worksheet.write('E1', 'Tasks Completed')
+
+#On bot end
+#@bot.event
+#async def on_close():
+    #workbook.close()
 
 
-
-
-#Bot commands
-
+#BOT COMMAND LIST
 #TEST
 @bot.command()
 async def hello(ctx):
     await ctx.send("Hi!")
+
+
+
+
+
+
 
 #Register user class
 def registration(name):
@@ -66,15 +93,21 @@ async def register(ctx, name):
 
 
 
+
+#Print commands to user
+@bot.command()
+async def commands_help(ctx):
+    await ctx.send("!commands_help")
+    await ctx.send("uploads list of commands to server chat")
+    await ctx.send("!register (user name)")
+    await ctx.send("registers user name to excel file to keep track of points")
+    await ctx.send("!print")
+    await ctx.send("prints excel file to view users, points, tasks with timestamps")
+
 #print excel file
 @bot.command()
 async def print(ctx):
     await ctx.send(file = discord.File('RegUsers.xlsx'))
 
-
-
-
-
-
 #bot token
-bot.run("MTE4MTI2NTc0MDE2NzQwMTU4Mw.GY2-9k.cb4yL9-qtTLj9SHdhtkcT1JVQhSdQoKZAJpGqg")
+bot.run("MTE4MTI2NTc0MDE2NzQwMTU4Mw.GHos_s.xCNY821leZNf_shp-AMeCs6UTEYD18aFdg1ETg")
